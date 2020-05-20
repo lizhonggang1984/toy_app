b00e89e5dff8e8b9c8a998fbe613ead2d17889ad
lizhonggang1984 <lizhonggang80@gmail.com>
2020-05-21 04:02:37 +0800

    finish user signup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_19_190550) do
+ActiveRecord::Schema.define(version: 2020_05_20_185346) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
@@ -21,10 +21,10 @@ ActiveRecord::Schema.define(version: 2020_05_19_190550) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "password"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
